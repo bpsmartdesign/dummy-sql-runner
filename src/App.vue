87073fe61e1c9ac333cc7b3758ae4781b2a3856d
BIYA Paul <paul@workerly.io>
2022-09-15 00:07:43 +0100

    refactor: Review dark bg.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+  <main font-sans class="w-screen h-screen text-gray-700 dark:text-gray-200">
+    <Header />
     <router-view />
     <Footer />
   </main>
