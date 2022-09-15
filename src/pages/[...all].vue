6f6354b13472331d02ebda0d365ef51112c79cff
BIYA Paul <paul@workerly.io>
2022-09-15 05:22:56 +0100

    feat(ux): Update not found page.

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    Not Found
+  <div flex items-center justify-center style="height: 92vh">
+    <div text-center>
+      <h1 text-9xl>
+        404
+      </h1>
+      <p>
+        Not Found
+      </p>
+      <router-link text-orange-6 text-xs underline to="/">
+        Go back
+      </router-link>
+    </div>
   </div>
 </template>
